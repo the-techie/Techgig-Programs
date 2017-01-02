dc4ff3f63acc52c837152d28c1f7818ec7b001e8
Akhil <Akhi1@users.noreply.github.com>
2017-01-02 22:51:19 +0530

    Update FactorialDemo.java

--- Learn and Code Challenge/FactorialDemo.java
+++ Learn and Code Challenge/FactorialDemo.java
@@ -3,15 +3,15 @@
 import java.io.*;
 import java.util.*;
 public class FactorialDemo {
-   public static void main(String args[]) throws Exception {
-	//Write code here
-	Scanner sc=new Scanner(System.in);
-	int number=sc.nextInt();
-	int i=(number-1);
-		while(i>0){
-			number*=i;
-			i--;
-		}
-		System.out.println(number);
+    public static void main(String args[]) throws Exception {
+    //Write code here
+    Scanner sc=new Scanner(System.in);
+    int number=sc.nextInt();
+    int i=(number-1);
+	while(i>0){
+	    number*=i;
+	    i--;
 	}
+    System.out.println(number);
+}
 }
