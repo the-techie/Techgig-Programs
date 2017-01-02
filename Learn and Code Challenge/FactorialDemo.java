29ddcf2f2d48dee9e9b6c3136b8afaa1f4fb02b7
Akhil <Akhi1@users.noreply.github.com>
2017-01-02 22:49:23 +0530

    Create FactorialDemo.java

---
+++ Learn and Code Challenge/FactorialDemo.java
@@ -0,0 +1,17 @@
+* Read input from STDIN. Print your output to STDOUT*/
+
+import java.io.*;
+import java.util.*;
+public class FactorialDemo {
+   public static void main(String args[]) throws Exception {
+	//Write code here
+	Scanner sc=new Scanner(System.in);
+	int number=sc.nextInt();
+	int i=(number-1);
+		while(i>0){
+			number*=i;
+			i--;
+		}
+		System.out.println(number);
+	}
+}
